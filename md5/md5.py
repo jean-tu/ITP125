ff7c947200abe80f44ac61ac1973f9921ca3a780
Jean Tu <tujean27@gmail.com>
2016-11-28 22:42:15 -0800

    still at the same spot, and need to find out how to fime how long a function will take to hash the word

--- md5/md5.py
+++ md5/md5.py
@@ -1,8 +1,15 @@
 import md5
+import time # to be able to time the functions 
 #Generating the code to read in from the file
 
 #ask the user for the file with the information that is supposed to be read in
 
 #open up the file
-passwords = open('passwords.txt')
-print(passwords.read())
+passwords = open('passwords.txt', 'r') #'r' to make it a read only
+line = passwords.readline()
+while line != "":
+    print(line)
+    line = passwords.readline()
+
+#close the file
+passwords.close()
