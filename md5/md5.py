92a5aafca26f975de6a7c81db3f80373bab401a7
Jean Tu <tujean27@gmail.com>
2016-11-28 22:47:30 -0800

    figured out how to get the hashed terms of the words

--- md5/md5.py
+++ md5/md5.py
@@ -1,15 +1,16 @@
-import md5
-import time # to be able to time the functions 
+import hashlib
+import time # to be able to time the functions
 #Generating the code to read in from the file
 
 #ask the user for the file with the information that is supposed to be read in
-
+md5 = hashlib.md5
 #open up the file
 passwords = open('passwords.txt', 'r') #'r' to make it a read only
 line = passwords.readline()
 while line != "":
-    print(line)
+    print(hashlib.md5(line.encode('utf-8')).hexdigest()) #hashing the password string
     line = passwords.readline()
+
 
 #close the file
 passwords.close()
