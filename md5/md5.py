466ed514c9b0fce9eb9a5dda7b2ec0bdc80d570b
Jean Tu <tujean27@gmail.com>
2016-11-28 22:10:38 -0800

    figured out how to read in a file, but need to work on why it's printing out double the content from the file

--- md5/md5.py
+++ md5/md5.py
@@ -1,2 +1,8 @@
 import md5
-#Generating the code to read in from the file 
+#Generating the code to read in from the file
+
+#ask the user for the file with the information that is supposed to be read in
+
+#open up the file
+passwords = open('passwords.txt')
+print(passwords.read())
