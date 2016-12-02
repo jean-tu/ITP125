71b9ba9b41ccd7cf8138b7fdc7a57f62b7e80325
Jean Tu <tujean27@gmail.com>
2016-12-01 17:38:53 -0800

    found out how to get the current time, but still have to figure out how to subtract the time

--- md5/md5.py
+++ md5/md5.py
@@ -1,5 +1,5 @@
 import hashlib
-import time # to be able to time the functions
+from datetime import datetime # to be able to time the functions
 #Generating the code to read in from the file
 
 #ask the user for the file with the information that is supposed to be read in
@@ -7,10 +7,14 @@ md5 = hashlib.md5
 #open up the file
 passwords = open('passwords.txt', 'r') #'r' to make it a read only
 line = passwords.readline()
+start_Time = str(datetime.now().time())
+print(start_Time)
 while line != "":
-    print(hashlib.md5(line.encode('utf-8')).hexdigest()) #hashing the password string
+    print(line, hashlib.md5(line.encode('utf-8')).hexdigest()) #hashing the password string
     line = passwords.readline()
-
+end_Time = str(datetime.now().time())
+print(end_Time)
+#print(end_Time - start_Time)
 
 #close the file
 passwords.close()
