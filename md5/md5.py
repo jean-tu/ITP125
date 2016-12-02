4d4d4289f801fa65853018e4178ebba39de46361
Jean Tu <tujean27@gmail.com>
2016-12-01 17:49:32 -0800

    found out how to use the time subtraction

--- md5/md5.py
+++ md5/md5.py
@@ -7,14 +7,22 @@ md5 = hashlib.md5
 #open up the file
 passwords = open('passwords.txt', 'r') #'r' to make it a read only
 line = passwords.readline()
-start_Time = str(datetime.now().time())
-print(start_Time)
+startTime = datetime.now().time()
+startTime_Second = float(str(startTime.second))
+startTime_Milli = float(str(startTime.microsecond))
+print(startTime_Second)
 while line != "":
     print(line, hashlib.md5(line.encode('utf-8')).hexdigest()) #hashing the password string
     line = passwords.readline()
-end_Time = str(datetime.now().time())
-print(end_Time)
-#print(end_Time - start_Time)
+endTime = datetime.now().time()
+endTime_Second = float(str(endTime.second))
+endTime_Milli = float(str(endTime.microsecond))
+
+
+if startTime_Second - endTime_Second > 0:
+    print(startTime_Second - endTime_Second)
+print("Microseconds: " , endTime_Milli - startTime_Milli)
+
 
 #close the file
 passwords.close()
