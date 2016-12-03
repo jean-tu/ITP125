0667c92a366428e595f348699d09197d587aa84d
Jean Tu <tujean27@gmail.com>
2016-12-03 10:50:57 -0800

    re-ordered the functions around, but have to figure out to pass variables because the math comes to 0 on time

--- md5/md5.py
+++ md5/md5.py
@@ -2,15 +2,10 @@ import hashlib
 from datetime import datetime # to be able to time the functions
 #Generating the code to read in from the file
 
-def guessPass(hashedPass):
-    startTimer()
-    print('hashed', hashedPass)
-    for i in xrange(0, index): #loop through all the possible character slots
-        #loop through the string.printable = 128 length
-        for j in xrange(0, 128):
-            while(hashed != guessed)
-        #endTimer()
-
+startTime_Second = 0
+startTime_Milli = 0
+endTime_Second = 0
+endTime_Milli = 0
 
 def startTimer():
     startTime = datetime.now().time()
@@ -24,6 +19,23 @@ def endTimer():
     endTime_Milli = float(str(endTime.microsecond))
     print("end timer")
 
+def guessPass(hashedPass):
+    startTimer()
+    print('hashed', hashedPass)
+    for i in xrange(0, index): #loop through all the possible character slots
+        #loop through the string.printable = 128 length
+        for j in xrange(0, 128):
+            guessed = hashlib.md5("Z".encode()).hexdigest()
+            if(hashedPass == guessed):
+                print("CORRECT")
+            #while(hashedPass != guessed):
+
+        endTimer()
+
+
+
+
+
 #main function
 md5 = hashlib.md5
 #open up the file
@@ -34,16 +46,13 @@ index = 0; #to keep track of how many characters are going to be in the string
 while line != "":
     print(line)
     index += 1
+
     guessPass(line) #call on the function to do the hashing
     #print(line, hashlib.md5(line.encode('utf-8')).hexdigest()) #hashing the password string
     line = passwords.readline().rstrip()
-
-
-
-"""if startTime_Second - endTime_Second > 0:
+if startTime_Second - endTime_Second > 0:
     print(startTime_Second - endTime_Second)
 print("Microseconds: " , endTime_Milli - startTime_Milli)
 
-"""
 #close the file
 passwords.close()
