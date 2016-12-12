9c9ba9eac91a223a4a1da6dd669ac3583c0ad8f1
Jean Tu <tujean27@gmail.com>
2016-12-12 11:16:34 -0800

    completed the hash, not that efficient for first draft and must clean up code

--- md5/md5.py
+++ md5/md5.py
@@ -1,4 +1,5 @@
 import hashlib
+import itertools
 from datetime import datetime # to be able to time the functions
 #Generating the code to read in from the file
 
@@ -26,40 +27,43 @@ def endTimer():
     endTime_Milli = float(str(endTime.microsecond))
     print(" end timer " + str(endTime_Second) + " " + str(endTime_Milli))
 
-def guessPassHelper(index, guessed, hashedPass):
-    global alphabetSpecial, characters
-    print hashlib.md5(guessed.encode()).hexdigest())
+def guessPassHelper(guessed, hashedPass):
+    global alphabetSpecial
+    print guessed + " " + hashlib.md5(guessed.encode()).hexdigest()
     if hashlib.md5(guessed.encode()).hexdigest() == hashedPass:
-        print "FOUND" + hashlib.md5(guessed.encode()).hexdigest()
+        print "FOUND " + hashlib.md5(guessed.encode()).hexdigest()
         return #you've found the hash
     else:
-       while index <= characters:
-            index += 1 #increment for the guessed character
-            """for i in alphabetSpecial:
-                guessed = guessed +
-                guessPassHelper(index, guessed, hashedPass) #recursive call
-"""
+        perms =  list(map("".join, itertools.permutations(alphabetSpecial, characters))) #list(itertools.permutations(alphabetSpecial, 2))
+    for per in perms:
+        if hashlib.md5(per.encode()).hexdigest() == hashedPass:
+            print "FOUND " + per +  " " + hashlib.md5(per.encode()).hexdigest()
+            return #you've found the hash
+
+
 def guessPass(hashedPass):
-    global alphabetSpecial
+    global alphabetSpecial, characters
     startTimer()      #call on start timer to grab time
     print('hashed', hashedPass)
-    index = 0
-    guessed = hashlib.md5(alphabetSpecial[index].encode()).hexdigest()
-    index += 1
-    guessPassHelper(index, guessed, hashedPass) #calling on the helper function
+    perms =  list(map("".join, itertools.permutations(alphabetSpecial, characters))) #list(itertools.permutations(alphabetSpecial, 2))
+    for per in perms:
+        if hashlib.md5(per.encode()).hexdigest() == hashedPass:
+            print "FOUND " + per +  " " + hashlib.md5(per.encode()).hexdigest()
+            return #you've found the hash
     endTimer()
+    #guessed = alphabetSpecial[index]
+    #guessPassHelper(guessed, hashedPass) #calling on the helper function
+
+
 
 
 #main function
 #open up the file
 passwords = open('hash.txt', 'r') #'r' to make it a read only
 line = passwords.readline().rstrip() #grabbing the first string to guess
-index = 0; #to keep track of how many characters are going to be in the string
-           # for the hash guesser
 while line != "":
     print line + " " + str(characters)
     guessPass(line) #call on the function to do the hashing
-    """print(line, hashlib.md5(line.encode('utf-8')).hexdigest()) #hashing the password string"""
     line = passwords.readline().rstrip()
     if startTime_Second - endTime_Second > 0:
         print("Seconds" + str(startTime_Second - endTime_Second))
